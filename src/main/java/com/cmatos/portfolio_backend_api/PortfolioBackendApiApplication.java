d3bb4faba42e2f1b3128377eee8f2d9ebe516ee5
carlos <carlinhos12345678962@gmail.com>
2026-01-28 11:37:36 -0300

    generates project with Spring Initializr

---
+++ src/main/java/com/cmatos/portfolio_backend_api/PortfolioBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.cmatos.portfolio_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmatos/portfolio_backend_api/PortfolioBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmatos.portfolio_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

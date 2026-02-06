567aae451f23eef3ba5a3a84c90a8f2881294b15
carlos <carlinhos12345678962@gmail.com>
2026-02-06 15:16:54 -0300

    fix `dependecies` attribute to `dependencies`

--- src/main/java/com/cmatos/portfolio_backend_api/records/HealthResponse.java
+++ src/main/java/com/cmatos/portfolio_backend_api/records/HealthResponse.java
@@ -2,5 +2,5 @@ package com.cmatos.portfolio_backend_api.records;
 
 import java.util.Map;
 
-public record HealthResponse(String updated_at, Map<String, DatabaseStatus> dependecies) {
+public record HealthResponse(String updated_at, Map<String, DatabaseStatus> dependencies) {
 }

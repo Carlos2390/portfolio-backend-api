dc09c6fa23eed9c85d4eda1efe7bf302eb5514b2
carlos <carlinhos12345678962@gmail.com>
2026-02-21 10:56:08 -0300

    validate URL format

--- src/main/java/com/cmatos/portfolio_backend_api/records/ProjectDTO.java
+++ src/main/java/com/cmatos/portfolio_backend_api/records/ProjectDTO.java
@@ -1,15 +1,20 @@
 package com.cmatos.portfolio_backend_api.records;
 
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
 
 import java.util.List;
 
 public record ProjectDTO(
         @NotNull
         String name,
+        @URL
         String documetationUrl,
+        @URL
         String interfaceUrl,
+        @URL
         String githubUrl,
+        @URL
         String imageUrl,
         String observations,
         List<SkillDTO> skills
